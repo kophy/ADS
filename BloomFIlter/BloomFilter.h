ac0da249b8f03c27a8ca4fbbf465575d9256660d
liquidconv <pitocin@163.com>
2016-02-10 18:02:58 +0800

    initial commit

---
+++ BloomFIlter/BloomFilter.h
@@ -0,0 +1,20 @@
+#ifndef __BLOOM_FILTER_H__
+#define __BLOOM_FILTER_H__
+
+#include <vector>
+#include <string>
+
+class BloomFilter {
+    public:
+        BloomFilter(unsigned int m, unsigned int k);
+        void insertElement(std::string s);
+        bool existsElement(std::string s);
+    private:
+        unsigned int BDKR_Hash(std::string s, unsigned int i);
+        unsigned int getSeed(unsigned int hash_index);
+        std::vector<bool> table;
+        unsigned int _m;
+        unsigned int _k;
+};
+
+#endif // __BLOOM_FILTER_H__
---
+++ BloomFIlter/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+#include "BloomFilter.h"
+
+using namespace std;
+
+int main() {
+    BloomFilter *bf = new BloomFilter(128, 10);
+    string data[] = {"Alice", "Bob", "Cathy", "Danny"};
+    
+    for(int i = 0; i < 4; ++i) {
+    	cout << "Inserting element: " << data[i] << endl;
+    	bf->insertElement(data[i]);
+    }
+	
+	cout << "Cathy is in the set: " << bf->existsElement("Cathy") << endl;
+	cout << "Eddie is in the set: " << bf->existsElement("Eddie") << endl;
+	
+    delete bf;
+    return 0;
+}
